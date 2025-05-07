69072a07b2bee54bbb54513de7de5da7c4273465
Jean-François Gamache <jeff.gamache@icloud.com>
2025-05-07 17:53:56 -0400

    Added scaling commands
    - Can generate command to scale a building down or up

--- src/app/components/block-display/block-display.component.ts
+++ src/app/components/block-display/block-display.component.ts
@@ -5,18 +5,31 @@ import { NbtDataService } from 'src/app/services/nbt-data.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { BlockCount, BlockData } from 'src/app/type';
 import { MatButtonModule } from '@angular/material/button';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-block-display',
-  imports: [CommonModule, MatListModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './block-display.component.html',
   styleUrl: './block-display.component.css',
 })
 export class BlockDisplayComponent {
   protected blockList: BlockCount[] = [];
   protected blockDataList: BlockData[] = [];
-  private command = 'setblock';
+  private command = 'scale';
   protected hasCommand = false;
+  protected scaleInput: FormControl<number> = new FormControl(1, {
+    nonNullable: true,
+  });
 
   @ViewChild('commandTextArea')
   commandTextArea!: ElementRef<HTMLTextAreaElement>;
@@ -35,8 +48,27 @@ export class BlockDisplayComponent {
       });
   }
 
-  //Curently only does setblock
+  //TODO: Create a strong builder that will don't require switch case what's over
   protected createCommands(): void {
+    let commands = '';
+    switch (this.command) {
+      case 'setblock': {
+        commands = this.setBlock();
+        break;
+      }
+      case 'scale': {
+        commands = this.scaleCommand();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+    this.commandTextArea.nativeElement.value = commands;
+  }
+
+  //Could move this to a service (Probably should)
+  private setBlock(): string {
     const commands: string[] = this.blockDataList.map(
       ({ block, position: { x, y, z }, property }) => {
         const entries = Object.entries(property ?? {})
@@ -51,5 +83,44 @@ export class BlockDisplayComponent {
     }
 
     console.log(commands);
+    return commands.join('\n');
+  }
+
+  private scaleCommand(): string {
+    const commands: string[] = this.blockDataList.map(
+      ({ block, position: { x, y, z }, property }) => {
+        const entries = Object.entries(property ?? {})
+          .map(([k, v]) => `${k}:"${v}"`)
+          .join(', ');
+
+        let properties = '';
+        if (entries) {
+          properties = `,Properties:{${entries}}`;
+        }
+
+        let scale = '';
+        if (this.scaleInput.value !== 1) {
+          scale = `,transformation:{left_rotation:[0f,0f,0f,1f],right_rotation:[0f,0f,0f,1f],translation:[0f,0f,0f],scale:[${this.scaleInput.value}f,${this.scaleInput.value}f,${this.scaleInput.value}f]}`;
+
+          x = parseFloat((x * this.scaleInput.value).toFixed(4));
+          y = parseFloat((y * this.scaleInput.value).toFixed(4));
+          z = parseFloat((z * this.scaleInput.value).toFixed(4));
+        }
+
+        return (
+          `summon block_display ~${x} ~${y} ~${z} {block_state:{Name:"${
+            block + '"' + properties
+          }}` +
+          scale +
+          `}`
+        );
+      }
+    );
+    if (commands.length) {
+      this.commandTextArea.nativeElement.value = commands.join('\n');
+    }
+
+    console.log(commands);
+    return commands.join('\n');
   }
 }
