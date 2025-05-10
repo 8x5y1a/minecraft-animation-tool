be26c5efd41b615e267a1501e551f62d5f6b0e54
Jean-François Gamache <jeff.gamache@icloud.com>
2025-05-10 14:43:06 -0400

    Command builder function
    - Created a stronger function to create the animation / commands without having too much duplicated code
    - Still a lot left of settings needs to be implemented / tweak

--- src/app/components/generate-command/generate-command.component.ts
+++ src/app/components/generate-command/generate-command.component.ts
@@ -31,85 +31,134 @@ export class GenerateCommandComponent {
       });
   }
 
-  //TODO: Create a strong builder that will don't require switch case what's over
   protected createCommands() {
     const properties: AnimationProperties = this.propertiesList[0];
     if (!properties) {
       return;
     }
-
-    console.log(properties);
-    let commandGenerated = '';
-    switch (properties.command.value) {
-      case 'set': {
-        commandGenerated = this.setBlock(properties);
-        break;
-      }
-      case 'display': {
-        commandGenerated = this.scaleCommand(properties);
-        break;
-      }
-      default: {
-        break;
-      }
+    if (properties.command.value === 'destroy') {
+      console.log('TODO:');
     }
+
+    const commandGenerated = this.buildCommands(properties);
+
     this.commandTextArea.nativeElement.value = commandGenerated;
   }
 
-  private setBlock(properties: AnimationProperties): string {
-    console.log(properties);
-    const commandGenerated: string[] = this.blockDataList.map(
-      ({ block, position: { x, y, z }, property }) => {
-        const entries = Object.entries(property ?? {})
-          .map(([k, v]) => `${k}=${v}`)
-          .join(', ');
-
-        return `setblock ~${x} ~${y} ~${z} ${block}[${entries}] replace`;
-      }
-    );
-    if (commandGenerated.length) {
-      this.commandTextArea.nativeElement.value = commandGenerated.join('\n');
+  /**
+   * Command generator helper function
+   * The function transforms the properties data into readable properties
+   * @param properties
+   * @returns
+   */
+  private buildPropertiesString(
+    properties: Record<string, string>,
+    isSet: boolean
+  ): string {
+    if (isSet) {
+      const propertiesEntries = Object.entries(properties)
+        .map(([k, v]) => `${k}=${v}`)
+        .join(',');
+      return propertiesEntries ? `[${propertiesEntries}]` : '';
     }
 
-    console.log(commandGenerated);
-    return commandGenerated.join('\n');
+    const propertiesEntries = Object.entries(properties)
+      .map(([key, value]) => `${key}:"${value}"`)
+      .join(', ');
+    return propertiesEntries ? `,Properties:{${propertiesEntries}}` : '';
   }
 
-  private scaleCommand(properties: AnimationProperties): string {
+  /**
+   * Command generator helper function
+   * The function transforms the transformation data based on the case (timing / not timing)
+   * @param properties
+   * @returns
+   */
+  private buildTransformation(
+    coords: [number, number, number],
+    isTiming: boolean,
+    scaleValue: number,
+    isDisplay: boolean
+  ): string {
+    if (!isDisplay || scaleValue === 1) {
+      return '';
+    }
+    const [x, y, z] = coords;
+    const translation = isTiming ? `[${x}f,${y}f,${z}f]` : `[0f,0f,0f]`;
+    return (
+      `,transformation:{` +
+      `left_rotation:[0f,0f,0f,1f],` +
+      `right_rotation:[0f,0f,0f,1f],` +
+      `translation:${translation},` +
+      `scale:[${scaleValue}f,${scaleValue}f,${scaleValue}f]}`
+    );
+  }
+
+  /**
+   * Command builder that will transform the settings into the commands
+   * @param properties
+   * @returns
+   */
+  private buildCommands(properties: AnimationProperties): string {
+    const isSet = properties.command.value === 'set';
+    const isDisplay = properties.command.value === 'display';
+    const isTiming = properties.timing.value;
+    const scaleValue = properties.scale.value;
+
     const commandGenerated: string[] = this.blockDataList.map(
       ({ block, position: { x, y, z }, property }) => {
-        const entries = Object.entries(property ?? {})
-          .map(([k, v]) => `${k}:"${v}"`)
-          .join(', ');
+        const propertiesString = this.buildPropertiesString(
+          property ?? {},
+          isSet
+        );
+        x = parseFloat((x * properties.scale.value).toFixed(4));
+        y = parseFloat((y * properties.scale.value).toFixed(4));
+        z = parseFloat((z * properties.scale.value).toFixed(4));
 
-        let propertieString = '';
-        if (entries) {
-          propertieString = `,Properties:{${entries}}`;
-        }
+        const timing = isTiming
+          ? `execute if score $Dataman count matches ${Math.round(y)} run`
+          : '';
+        const coordinates = isTiming ? `0 0 0` : `~${x} ~${y} ~${z}`;
+        const transform = this.buildTransformation(
+          [x, y, z],
+          isTiming,
+          scaleValue,
+          isDisplay
+        );
 
-        let scale = '';
-        if (properties.scale.value !== 1) {
-          scale = `,transformation:{left_rotation:[0f,0f,0f,1f],right_rotation:[0f,0f,0f,1f],translation:[0f,0f,0f],scale:[${properties.scale.value}f,${properties.scale.value}f,${properties.scale.value}f]}`;
-
-          x = parseFloat((x * properties.scale.value).toFixed(4));
-          y = parseFloat((y * properties.scale.value).toFixed(4));
-          z = parseFloat((z * properties.scale.value).toFixed(4));
+        if (isSet) {
+          return `${timing} setblock ${x} ${y} ${z} ${block}${propertiesString} replace`;
         }
 
         return (
-          `summon block_display ~${x} ~${y} ~${z} {block_state:{Name:"${
-            block + '"' + propertieString
-          }}` +
-          scale +
+          `${timing} summon block_display ${coordinates} {` +
+          `block_state:{Name:"${block}"${propertiesString}}` +
+          `${transform}` +
           `}`
         );
       }
     );
-    if (commandGenerated.length) {
-      this.commandTextArea.nativeElement.value = commandGenerated.join('\n');
-    }
 
+    if (properties.timing.value) {
+      const maxAnimationWay = 4; //TODO: Keep track of the max
+      const dataPackName = 'jeff:test';
+      commandGenerated.unshift(
+        'scoreboard objectives add count trigger',
+        'scoreboard players add $Dataman count 0'
+      );
+      commandGenerated.push(
+        `execute if score $Dataman count matches ..${maxAnimationWay} run scoreboard players add $Dataman count 1`,
+        `execute if score $Dataman count matches ..${maxAnimationWay} run schedule function ${dataPackName} ${properties.speed.value}`,
+        `execute if score $Dataman count matches ${
+          maxAnimationWay + 1
+        } run scoreboard players set $Dataman count 0`
+      );
+    }
+    const commandResult = commandGenerated.join('\n');
+    if (commandGenerated.length) {
+      this.commandTextArea.nativeElement.value = commandResult;
+    }
     console.log(commandGenerated);
-    return commandGenerated.join('\n');
+    return commandResult;
   }
 }
