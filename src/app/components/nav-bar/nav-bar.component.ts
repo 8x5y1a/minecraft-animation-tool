80f20085b51b661d6193534612bddfda464174e0
Jean-François Gamache <jeff.gamache@icloud.com>
2025-05-20 20:54:48 -0400

    Css for navigation
    - Added css for the navigation
    - Keeping it non sticky for now, since it ruins the automatic scrolling

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDivider } from '@angular/material/divider';
 import { RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [CommonModule, MatDivider, RouterLink],
+  imports: [CommonModule, MatDivider, RouterLink, MatIcon],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
   standalone: true,
