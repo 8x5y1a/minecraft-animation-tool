318b61483582ce9801de85855a4306ab71da55f4
Jean-François Gamache <jeff.gamache@icloud.com>
2025-05-26 20:49:49 -0400

    About page content
    - Added about page content (Still needs some work though)
    - Nav set to sticky

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButton, MatDivider, MatIcon, RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
   standalone: true,
 })
-export class AboutComponent {}
+export class AboutComponent {
+  protected readonly githubUrl =
+    'https://github.com/jeffgamache/minecraft-animation-tool';
+  protected readonly gneissChannelUrl = 'https://www.youtube.com/@gneissname';
+  protected readonly sheetUrl =
+    'https://docs.google.com/spreadsheets/d/15j8Jb_n9rYhQTGhOZa2U289ytoMmhe5HJ_BYetV7Zdg/edit?gid=167770427#gid=167770427';
+  protected readonly videoUrl =
+    'https://youtu.be/YbclYBh9n8I?si=avZBt6OKoWTqj2yF';
+
+  protected openGithub(path = ''): void {
+    window.open(this.githubUrl + path, '_blank');
+  }
+}
