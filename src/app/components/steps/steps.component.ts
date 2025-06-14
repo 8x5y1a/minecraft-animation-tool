eb7dcf4c0d1a33a43a96219e63c44090de1913e4
8x5y1a <jeff.gamache@icloud.com>
2025-06-13 23:23:26 -0400

    Change steps to Stepper
    - Changed accordion to use Stepper instead to make it easier to navigate / readability

--- src/app/components/steps/steps.component.ts
+++ src/app/components/steps/steps.component.ts
@@ -4,10 +4,11 @@ import {
   ElementRef,
   signal,
   ViewChild,
+  OnDestroy,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatStepperModule } from '@angular/material/stepper';
+import { MatStepper, MatStepperModule } from '@angular/material/stepper';
 import { BlockDisplayComponent } from '../block-display/block-display.component';
 import { AnimationSettingsComponent } from '../animation-settings/animation-settings.component';
 import { GenerateCommandComponent } from '../generate-command/generate-command.component';
@@ -15,9 +16,19 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { PreferenceService } from 'src/app/services/preference.service';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { MatDivider } from '@angular/material/divider';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-steps',
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false },
+    },
+  ],
   imports: [
     CommonModule,
     MatButtonModule,
@@ -28,20 +39,25 @@ import { PreferenceService } from 'src/app/services/preference.service';
     MatExpansionModule,
     MatIconModule,
     MatFormFieldModule,
+    MatDivider,
   ],
   templateUrl: './steps.component.html',
   styleUrl: './steps.component.css',
   standalone: true,
 })
-export class StepsComponent implements AfterViewInit {
+export class StepsComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('stepper', { read: ElementRef })
+  private stepperElement?: ElementRef<HTMLElement>;
+  @ViewChild('stepper')
+  private stepperComponent?: MatStepper;
+
   protected step = signal(0);
-  @ViewChild('blockList', { read: ElementRef })
-  blockList?: ElementRef<HTMLElement>;
+  private stepSub?: Subscription;
 
   constructor(protected preferenceService: PreferenceService) {}
 
   ngAfterViewInit() {
-    this.blockList?.nativeElement.scrollIntoView({
+    this.stepperElement?.nativeElement.scrollIntoView({
       behavior: 'smooth',
       inline: 'nearest',
       block: 'nearest',
@@ -49,17 +65,15 @@ export class StepsComponent implements AfterViewInit {
     if (this.preferenceService.skipBlockList) {
       this.step.set(1);
     }
+
+    this.stepSub = this.stepperComponent?.selectedIndexChange.subscribe(
+      (index) => {
+        this.step.set(index);
+      }
+    );
   }
 
-  protected setStep(index: number) {
-    this.step.set(index);
-  }
-
-  protected nextStep() {
-    this.step.update((i) => i + 1);
-  }
-
-  protected prevStep() {
-    this.step.update((i) => i - 1);
+  ngOnDestroy() {
+    this.stepSub?.unsubscribe();
   }
 }
